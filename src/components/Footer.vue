ba736dd1b10bf69d08ca1993e2a607eefb04ca84
SieChan <sie.chan@outlook.com>
2020-12-12 14:09:15 -0700

    edited page layouts

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="footer-container">
         <h4>QUEERTR</h4>
-        <h4>Copyright</h4>
+        <h4>Copyright 2020</h4>
         <h4>By Sie Chan</h4>
     </div>
 </template>
@@ -20,12 +20,15 @@
     grid-template-columns: 1fr 1fr 1fr;
     width: 100%;
     height: 10vh;
+    background-color: #7FB685;
     border-top: 2px solid #426A5A;
 }
 h4 {
-    
-    margin-top: 9vh;
-    margin-bottom: 5vh;
+    align-items: center;
+    justify-items: center;
+    padding: 5px;
+    /* margin-top: 9vh;
+    margin-bottom: 5vh; */
     font-size: 10px;
 }
 </style>
